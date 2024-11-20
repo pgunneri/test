5a7dff4e43ce77ab16cbe5bfc4f21f1a9ee22a1e
pgunneri <pgunneri@dwss.nv.gov>
2024-11-19 16:02:53 -0800

    Initial commit

---
+++ src/random/test.java
@@ -0,0 +1,10 @@
+package random;
+
+public class test {
+
+	    public static void main(String[] args) {
+	        // This line prints "Hello, World!" to the console
+	        System.out.println("Hello, World!");
+	    }
+	}
+
